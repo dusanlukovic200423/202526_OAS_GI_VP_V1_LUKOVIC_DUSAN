b599bd9964f53b2e687d504e80a8b6f41a22023c
Dusan Lukovic <dusanlukovic.lukovic3@gmail.com>
2025-11-12 20:49:54 +0100

    promena brisanje brisanje studenta

--- src/backend/app.py
+++ src/backend/app.py
@@ -2,4 +2,3 @@ print("Pozdrav sa backend-a!")
 print("Azurirani pozdrav sa backend-a!")
 #poslata je prva verzija
 #ovo je druga verzija
-print("Drugo azuriranje pozdrava sa beckend-a!")
--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,3 +1,4 @@
 console.log("Pozdrav sa forntend-a!")
 console.log("Azuriran pozdrav sa frontend-a!");
 //fajl je vracen na prvi commit uz pomoc restore komande
+console.log("Brisanje studenta je implementirano!")
